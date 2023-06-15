99c434813c57f1b98f704ef3f3501915fb46700b
LostoVayne <frankhx018@gmail.com>
2023-06-14 22:53:07 -0400

    fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import clock from '../assets/images/icon-clock.svg'
 import etherium from '../assets/images/icon-ethereum.svg'
+import view from '../assets/images/icon-view.svg'
 import avatar from '../assets/images/image-avatar.png'
 import imgprincipal from '../assets/images/image-equilibrium.jpg'
 const Card = () => {
   return (
     <div className='w-[20.625rem] h-[34.375rem] bg-[#15273F] rounded-2xl p-5'>
-      <img src={imgprincipal} alt='' className='rounded-lg' />
-      <h2 className='text-white mt-5 mb-2 text-xl font-semibold '>Equilibrium #3429</h2>
+      <div className='relative flex items-center justify-center group hover:bg-[#0AFFFF] transition-colors duration-500 rounded-lg cursor-pointer'>
+        <img src={view} alt='' className='absolute hidden group-hover:block' />
+        <img src={imgprincipal} alt='' className='rounded-lg group-hover:opacity-25' />
+      </div>
+      <h2 className='text-white mt-5 mb-2 text-xl font-semibold hover:text-[#0AFFFF]'>
+        Equilibrium #3429
+      </h2>
       <p className='text-gray-500 font-normal text-lg'>
         Our Equilibrium collection promotes balance and calm.
       </p>
@@ -23,7 +29,7 @@ const Card = () => {
 
       <footer className='flex items-center gap-4 mt-4'>
         <img src={avatar} alt='' className='w-9 ring-2 ring-white  h-9 rounded-full' />
-        <p className='text-[#8BAAD9]'>
+        <p className='text-[#8BAAD9] hover:text-[#0AFFFF]'>
           Creation of <span className='text-white'> Jules Wyvern</span>
         </p>
       </footer>
